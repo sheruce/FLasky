9019d4f8433fbe092c5bcc2612bf630f8cc9a67b
sheruce <sheruce@chess.com>
2019-03-17 18:00:10 +0800

    forth chapter

--- hello.py
+++ hello.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for,flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+app.config['SECRET_KEY'] = 'password'
 
 
-@app.route('/')
+class NameForm(FlaskForm):
+    name = StringField('Whats your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name=session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Look like you have changed your name.')
+        session['name']=form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', name=session.get('name'), form=form)
 
 
 @app.route('/user/<name>')
