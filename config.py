98e44b7456205927160d0d7e7744b1790248d2c2
sheruce <sheruce@chess.com>
2019-04-14 17:24:19 +0800

    10.3

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ class Config:
     MAIL_USERNAME = os.environ.get('batma52@163.com')
     MAIL_PASSWORD = os.environ.get('zxcqwe123')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <batma52@163.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <sheruce221@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
