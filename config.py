299a69f768b31e15f00e9d2c87fcce8a2f46ecb5
sheruce <sheruce@chess.com>
2019-04-20 15:18:43 +0800

    12.2

--- config.py
+++ config.py
@@ -15,6 +15,7 @@ class Config:
     FLASKY_ADMIN = 'glorsy@163.com'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     FLASKY_POSTS_PER_PAGE = 20
+    FLASKY_FOLLOWERS_PER_PAGE = 50
 
     @staticmethod
     def init_app(app):
